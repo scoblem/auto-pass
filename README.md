d2e268f95e6fcab5737293e318411dee88cb2a6e
Mark Scoble <scoble.mr@gmail.com>
2015-10-04 22:43:39 +0800

    Moved email credentials to json file Added get_user function Added HTML messages

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-# auto-pass
+# auto_pass
 
 Generate a random password, assign it to a user & email it to them.
 
 * Must be run with appropriate privileges.
 * If using gmail, enable 2 factor authentication and app password.
+* email_cred.json = {"default-mail": ["<email_address>", "<password>"], "alt-mail": ["<alt_address>", "<password>"]} etc...
+
 
 ####TODO:
 
-1. Get current user.
+1. Get current user. ~ done.
 2. Add command line arguments (argparse) (user, password strength, logging).
-3. Move password gen to function.
-4. Store email credentials in json.
-5. Add subject / html email.
+3. Move password gen to function. ~ done.
+4. Store email credentials in json. ~ done.
+5. Add subject / html email. ~ done.
 6. Test as cron job on logoff.
 7. ???
 8. Profit!
--- auto_pass.py
+++ auto_pass.py
@@ -1,51 +1,61 @@
 # what's the elvish word for friend?
 
-import os, subprocess, smtplib
+import os, subprocess, smtplib, json
 from base64 import b64encode
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-from_a = '<FromEmail>'
-to_a = '<ToEmail>'
-msg = """
-Your new password is: {0}
-
-Keep it secret, keep it safe.
-"""
-
-def get_user():
+def get_user(): # Returns the currently logged in user.
     p = subprocess.check_output({'logname'})
     username = p.strip()
-    if username != 'root': # just in case...
+    if username != 'root': # Just in case...
         return username
 
-def gen_passwd(n=12):
+def gen_passwd(n=12): # Returns a random password using /dev/urandom of n size (default 12 bytes)
     random_bytes = os.urandom(n)
-    encoded_passwd = b64encode(random_bytes).decode('utf-8')
+    random_passwd = b64encode(random_bytes).decode('utf-8')
+    return random_passwd
 
-    return encoded_passwd
-
-def change_passwd(user, passwd):
+def change_passwd(user, passwd): # Pass user:passwd to chpasswd
     chpasswd = subprocess.Popen(['chpasswd'],
                             stdin=subprocess.PIPE)
+    chpasswd.communicate("{USER}:{PASSWD}".format(USER=user, PASSWD=passwd))
 
-    chpasswd.communicate("{0}:{1}".format(user, passwd))
+def get_mail_cred(address_key='default-mail'): # Returns email credentials from email_cred.json
+    with open('email_cred.json', 'r') as f:
+        address_dict = json.load(f)
+    return (address_dict[address_key][0], address_dict[address_key][1])
 
-def gen_msg():
-    pass
+def gen_msg(user, passwd, FROM, TO): # Returns HTML email
+    msg_content = """
+    <p>Hi {USER}! Your new password is:<b> {PASSWD}</b></p>
+    <p>Keep it secret, keep it safe!</p>""".format(USER=user, PASSWD=passwd)
 
-def send_mail(from_a, to_a, msg):
-    username = "<FromEmail>"
-    password = "<YourPassword>"
+    message = MIMEText(msg_content, 'html')
+    message['From'] = 'Password Robot <{FROM}>'.format(FROM=FROM)
+    message['To'] = '{USER} <{TO}>'.format(USER=user, TO=TO)
+    message['Subject'] = 'New password for {USER}!'.format(USER=user)
+
+    msg_full = message.as_string()
+    return msg_full
+
+def send_mail(mail_user, mail_pass, FROM, TO, MSG): # Send mail via smtplib / gmail
     server = smtplib.SMTP_SSL('smtp.gmail.com:465')
-    server.login(username, password)
-    server.sendmail(from_a, to_a, msg.format(set_passwd))
+    server.login(mail_user, mail_pass)
+    server.sendmail(FROM, TO, MSG)
     server.quit()
 
 if __name__ == ("__main__"):
     # Mellon
     username = get_user()
     random_passwd = gen_passwd()
-
     change_passwd(username, random_passwd)
-    # send_mail(from_a, to_a, msg)
-    print(username)
-    print(random_passwd)
+    mail_user, mail_pass = get_mail_cred()
+
+    FROM = mail_user
+    TO = '<To@Adress.com>'
+    MSG = gen_msg(username, random_passwd, FROM, TO)
+
+    print("Connecting...")
+    send_mail(mail_user, mail_pass, FROM, TO, MSG)
+    print('Password sent to {user} <{email}>'.format(user=username, email=TO))
