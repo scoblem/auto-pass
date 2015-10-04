2ca97394c0c60dba001cf08751c6bfcdc04c7edf
Mark Scoble <scoble.mr@gmail.com>
2015-10-04 19:35:22 +0800

    Added get_user function, renamed script to allow import.

--- auto_pass.py
+++ auto_pass.py
@@ -3,9 +3,6 @@
 import os, subprocess, smtplib
 from base64 import b64encode
 
-username = "localuser"
-set_passwd = b64encode(os.urandom(64)).decode('utf-8') # Mellon
-
 from_a = '<FromEmail>'
 to_a = '<ToEmail>'
 msg = """
@@ -14,14 +11,29 @@ Your new password is: {0}
 Keep it secret, keep it safe.
 """
 
+def get_user():
+    p = subprocess.check_output({'logname'})
+    username = p.strip()
+    if username != 'root': # just in case...
+        return username
+
+def gen_passwd(n=12):
+    random_bytes = os.urandom(n)
+    encoded_passwd = b64encode(random_bytes).decode('utf-8')
+
+    return encoded_passwd
+
 def change_passwd(user, passwd):
     chpasswd = subprocess.Popen(['chpasswd'],
                             stdin=subprocess.PIPE)
 
     chpasswd.communicate("{0}:{1}".format(user, passwd))
 
+def gen_msg():
+    pass
+
 def send_mail(from_a, to_a, msg):
-    username = "<<FromEmail>>"
+    username = "<FromEmail>"
     password = "<YourPassword>"
     server = smtplib.SMTP_SSL('smtp.gmail.com:465')
     server.login(username, password)
@@ -29,5 +41,11 @@ def send_mail(from_a, to_a, msg):
     server.quit()
 
 if __name__ == ("__main__"):
-    change_passwd(username, set_passwd)
-    send_mail(from_a, to_a, msg)
+    # Mellon
+    username = get_user()
+    random_passwd = gen_passwd()
+
+    change_passwd(username, random_passwd)
+    # send_mail(from_a, to_a, msg)
+    print(username)
+    print(random_passwd)
